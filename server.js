a4f4b480273ab3bc99ddc3946a5db61596d997c4
lenovo <lenovo@LAPTOP-BLNTEBH7>
2023-05-07 09:15:19 +0530

    sad

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ db.mongoose
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to weltec final react test." });
 });
 
 require("./app/routes/turorial.routes")(app);
